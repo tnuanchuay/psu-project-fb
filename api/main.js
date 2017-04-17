1f0dff88c069caaf50afba765a59e842c1517d89
Tossapon Nuanchuay <Tossapon.Nuanchuay@agoda.com>
2017-04-17 20:53:07 +0700

    add test param in config

--- api/main.js
+++ api/main.js
@@ -1,14 +1,14 @@
 module.exports = main
 var error = require("../utils/onerror");
-function main(config, db, measurement){
+function main(config, db, measurement) {
     var express = require("express");
     var bodyParser = require("body-parser");
     var app = express();
     var posts = db.collection("posts");
     var detail = db.collection("post_details");
-    app.use(bodyParser.urlencoded({extended:false}));
+    app.use(bodyParser.urlencoded({ extended: false }));
 
-    app.use(function(req, res, next){
+    app.use(function (req, res, next) {
         measurement.api.numberOfRequest.increment();
         next();
     });
@@ -18,20 +18,22 @@ function main(config, db, measurement){
         next();
     });
 
-    app.get("/posts", function(req, res){
-        detail.find({}).toArray((err, array)=>{
+    app.get("/posts", function (req, res) {
+        detail.find({}).toArray((err, array) => {
             error(err);
             res.end(JSON.stringify(array));
         })
     });
 
-    app.get("/version", function(req, res){
-        res.end(JSON.stringify({"version":config.version.toString()}));
+    app.get("/version", function (req, res) {
+        res.end(JSON.stringify({ "version": config.version.toString() }));
     });
 
-    app.get("/token", function(req, res){
-        res.end(config.graphToken)
-    })
+    if (config.test) {
+        app.get("/token", function (req, res) {
+            res.end(config.graphToken)
+        })
+    }
 
     app.listen(config.api.port);
 }
--- utils/config.js
+++ utils/config.js
@@ -1,23 +1,24 @@
 module.exports = {
-	"graphToken" : "",
-	"secret" : "bccc5655ee238576064f1ae79c731fae",
-	"clientId" : "1920667534830924",
-	"db":{
-		"host":"localhost",
-		"port":27017,
-		"dbname":"psu-project"
+	"graphToken": "",
+	"secret": "bccc5655ee238576064f1ae79c731fae",
+	"clientId": "1920667534830924",
+	"db": {
+		"host": "localhost",
+		"port": 27017,
+		"dbname": "psu-project"
 	},
-	"version":"",
-	"api":{
-		"port":8000
+	"version": "",
+	"api": {
+		"port": 8000
 	},
-	init:function(){
+	"test": true,
+	init: function () {
 		var fs = require('fs');
 		this.graphToken = fs.readFileSync("token.conf");
 		this.version = fs.readFileSync("version.conf");
 		return this;
 	},
-	writeNewToken: function(token){
+	writeNewToken: function (token) {
 		var fs = require('fs');
 		fs.writeFileSync("token.conf", token);
 	}
