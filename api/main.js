25f63a91c21df1d82bc58dd2f8c3d3245a64f419
Tossapon Nuanchuay <Tossapon.Nuanchuay@agoda.com>
2017-04-20 22:45:45 +0700

    hotfix

--- api/main.js
+++ api/main.js
@@ -8,7 +8,7 @@ function main(config, db, measurement) {
     var detail = db.collection("post_details");
     app.use(bodyParser.urlencoded({ extended: false }));
     app.use(function (req, res, next) {
-        res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+        res.setHeader('Access-Control-Allow-Origin', '*');
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
         res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
         res.setHeader('Access-Control-Allow-Credentials', true);
